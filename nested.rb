d118175f373841ff53b246787fdd30d3959708fb
Jesse Brown <brownjesse8895@yahoo.com>
2021-02-03 15:44:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,12 +15,10 @@ def hopper
         :languages => ["C"]
       }
     }
-
-end
+    programmer_hash[:grace_hopper]
+end 
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -36,7 +34,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +53,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -67,9 +65,13 @@ def adding_matz
 # return the entire updated hash
 
 	programmer_hash = 
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
+	{
+	  :yukihiro_matsumoto => { 
+	    :kown_for => "ruby",
+	:languages => ["LISP", "C"]
+	},
+ 		  :grace_hopper => {
+ 		    :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
       },
       :alan_kay => {
@@ -81,7 +83,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash
 end
 
 def changing_alan
@@ -103,8 +105,12 @@ def changing_alan
         :languages => ["C"]
       }
     }
+programer_hash[:alan_kay][:known_for] = "GUI"
 
+programer_hash
 end
+
+
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
